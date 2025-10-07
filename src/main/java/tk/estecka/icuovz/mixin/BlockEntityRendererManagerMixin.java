c3a21d8de46e78bd027d14373ddc58b0a5fb21b6
CommandGenius <rvlcariati@gmail.com>
2025-10-07 14:05:16 -0400

    Update to 1.21.10

--- src/main/java/tk/estecka/icuovz/mixin/BlockEntityRendererManagerMixin.java
+++ src/main/java/tk/estecka/icuovz/mixin/BlockEntityRendererManagerMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.render.block.entity.BlockEntityRenderManager;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.util.math.Vec3d;
 import static tk.estecka.icuovz.ISeeYouOverThereMod.CONFIG;
@@ -17,11 +17,11 @@ import static tk.estecka.icuovz.ISeeYouOverThereMod.fovTan;
  * mod "Enhanced Block Entities", which does the same thing but better.
  */
 @Unique
-@Mixin(BlockEntityRenderDispatcher.class)
-public class BlockEntityRendererDispatcherMixin 
+@Mixin(BlockEntityRenderManager.class)
+public class BlockEntityRendererManagerMixin
 {
-	@WrapOperation( method="render", at=@At(value="INVOKE", target="net/minecraft/client/render/block/entity/BlockEntityRenderer.isInRenderDistance (Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/util/math/Vec3d;)Z") )
-	private boolean AdjustRenderDistance(BlockEntityRenderer<?> renderer, BlockEntity block, Vec3d camera, Operation<Boolean> original){
+	@WrapOperation( method="getRenderState", at=@At(value="INVOKE", target="net/minecraft/client/render/block/entity/BlockEntityRenderer.isInRenderDistance (Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/util/math/Vec3d;)Z") )
+	private boolean AdjustRenderDistance(BlockEntityRenderer<?, ?> renderer, BlockEntity block, Vec3d camera, Operation<Boolean> original){
 		return block.getPos().getSquaredDistance(camera)*fovTan*fovTan <= CONFIG.blockMin*(double)CONFIG.blockMin
 		    || original.call(renderer, block,  block.getPos().toCenterPos().lerp(camera, fovTan))
 		    ;
